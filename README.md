426dda50ced36e662af2901a04a0bdc281654037
xiangruicheng <xiangruicheng@superatomfin.com>
2024-04-28 15:24:29 +0800

    modify readme

--- README.md
+++ README.md
@@ -5,12 +5,15 @@
 ddl.Create()
 
 ### 2 Init Data
-* Init stock info
+* Init stock info 
 ```
 datainit.InitStockInfo()
 ```
-
-* Init kline
+* Init all kline 
 ```
 datainit.BatchInitKline()
+```
+* Init kline for an code
+```
+ainit.InitKline(model.StockInfoModel_TypeEtf, "159915", "SZ", config.Data.Xueqiu.InitNum)
 ```
--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"gostock/config"
-	"gostock/data/ddl"
+	"gostock/data/datainit"
+	"gostock/model"
 	"gostock/server"
 )
 
@@ -11,16 +12,6 @@ func main() {
 	server.InitRedis()
 	server.InitMysql()
 
-	ddl.Create()
-
-	//server.Log(server.LogLevelDebug, "this is error")
-
-	//datainit.BatchInitKline()
-	//datainit.InitStockInfo()
-	//datainit.InitKline("515790", "SH", config.Data.Xueqiu.InitNum)
-	//datainit.InitKline("159915", "SZ", config.Data.Xueqiu.InitNum)
-	//datainit.InitKline("000001", "SH", config.Data.Xueqiu.InitNum)
-	//datainit.InitKline("399001", "SZ", config.Data.Xueqiu.InitNum)
-	//datainit.InitKline("399006", "SZ", config.Data.Xueqiu.InitNum)
+	datainit.InitKline(model.StockInfoModel_TypeEtf, "159915", "SZ", config.Data.Xueqiu.InitNum)
 
 }
