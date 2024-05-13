e42df9c789fc6aa549dd0861d0bbf60bbb9f7900
xiangruicheng <xiangruicheng@superatomfin.com>
2024-05-13 19:01:59 +0800

    format printf

--- go.mod
+++ go.mod
@@ -12,4 +12,8 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- report/real.go
+++ report/real.go
@@ -2,6 +2,7 @@ package report
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"gostock/data/datasource/xueqiu"
 )
 
@@ -11,10 +12,16 @@ var RealPeportConfig = [][]string{
 }
 
 func Real() {
+	fmt.Printf("%10s|%10s|%10s|%10s|\n", "TAG", "PRICE", "PERCENT", "CHG")
+	fmt.Printf("%10s|%10s|%10s|%10s|\n", "----------", "----------", "----------", "----------")
 	for _, item := range RealPeportConfig {
 		tag := item[0]
 		symbol := item[1]
 		quote, _ := xueqiu.Quote(symbol)
-		fmt.Printf("\t%s\t|\t%f\t|\t%f\t|\t%f\t\n", tag, quote.Data.Quote.Current, quote.Data.Quote.Percent, quote.Data.Quote.Chg)
+		if quote.Data.Quote.Chg > 0 {
+			color.Red("%10s|%10f|%10f|%10f|\n", tag, quote.Data.Quote.Current, quote.Data.Quote.Percent, quote.Data.Quote.Chg)
+		} else {
+			color.Green("%10s|%10f|%10f|%10f|\n", tag, quote.Data.Quote.Current, quote.Data.Quote.Percent, quote.Data.Quote.Chg)
+		}
 	}
 }
