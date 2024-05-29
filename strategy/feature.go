9079fa195de77387f605510edd6f2362891a2d67
xiangruicheng <xiangruicheng@superatomfin.com>
2024-05-29 18:18:32 +0800

    xg

--- strategy/feature.go
+++ strategy/feature.go
@@ -24,6 +24,20 @@ func (f *FeatureStruct) IsMacdGold(code string, date string) bool {
 	return false
 }
 
+func (f *FeatureStruct) IsMacdDie(code string, date string) bool {
+	currMacd, _ := new(model.MacdModel).GetByCodeAndDate(code, date)
+	nextTradeDay := TradeDay.NextTradeDate(date, 1)
+	if nextTradeDay == "" {
+		return false
+	}
+	nextMacd, _ := new(model.MacdModel).GetByCodeAndDate(code, nextTradeDay)
+
+	if nextMacd.Macd < 0 && currMacd.Macd > 0 {
+		return true
+	}
+	return false
+}
+
 func (f *FeatureStruct) IsCrossStar(code string, date string) bool {
 	kline, _ := new(model.KlineModel).GetByCodeAndDate(code, date)
 	if math.Abs(kline.Open-kline.Close)/(kline.High-kline.Low) < 0.0001 {
